6d086fbc7d6be7a5e545de67d8de89fbe0c208f7
Neelanjana Mukherjee <neelanjanamukherjee@zipcoders-MacBook-Pro.local>
2019-02-14 17:19:31 -0500

    added total 5 person attributes and their test cases

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -1,5 +1,7 @@
 package com.zipcodewilmington.person;
 
+import java.util.Date;
+
 /**
  * Created by leon on 2/12/18.
  */
@@ -7,29 +9,108 @@ public class Person {
     private String name;
     private int age;
 
+    private String gender;
+    private Date birthDay;
+    private String maritalStatus;
+
+
+
     public Person() {
+
+        this.name = "";
+        this.age = Integer.MAX_VALUE;
+
+
     }
 
     public Person(int age) {
+
+        this.age =age;
     }
 
     public Person(String name) {
+
+        this.name = name;
     }
 
     public Person(String name, int age) {
+
+        this.age = age;
+        this.name =name;
+    }
+
+    public Person(String name, int age, String maritalStatus, String gender, Date birthDay) {
+
+        this.age = age;
+        this.name =name;
+        this.birthDay = birthDay;
+        this.gender =gender;
+        this.maritalStatus = maritalStatus;
     }
 
     public void setName(String name) {
+
+        this.name = name;
+
     }
 
     public void setAge(int age) {
+
+        this.age = age;
+
+
     }
 
     public String getName() {
-        return null;
+
+
+        return name;
     }
 
     public Integer getAge() {
-        return null;
+
+
+        return age;
+    }
+
+    public void setMaritalStatus(String maritalStatus) {
+
+
+        this.maritalStatus = maritalStatus;
+    }
+
+
+    public String getMaritalStatus() {
+
+
+        return maritalStatus;
+    }
+
+    public void setGender(String gender) {
+
+
+        this.gender = gender;
+    }
+
+
+
+    public String getGender() {
+
+
+        return gender;
+    }
+
+    public void setBirthDay(Date birthDay) {
+
+
+        this.birthDay = birthDay;
+    }
+
+
+
+    public Date getBirthDay() {
+
+
+        return birthDay;
     }
 }
--- src/test/java/com/zipcodewilmington/person/TestPerson.java
+++ src/test/java/com/zipcodewilmington/person/TestPerson.java
@@ -3,6 +3,8 @@ package com.zipcodewilmington.person;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Date;
+
 /**
  * Created by leon on 2/12/18.
  */
@@ -95,4 +97,74 @@ public class TestPerson {
         Integer actual = person.getAge();
         Assert.assertEquals(expected, actual);
     }
+
+    @Test
+    public void testConstructorWithAllFive() {
+        // Given
+        Integer expectedAge = 5;
+        String expectedName = "Adrish";
+        String expectedMaritalStatus = "Not Married";
+        String expectedGender = "Male";
+        Date expectedBirthDay = new Date(2);
+
+        // When
+        Person person = new Person(expectedName, expectedAge, expectedMaritalStatus, expectedGender, expectedBirthDay);
+
+        // Then
+        Integer actualAge = person.getAge();
+        String actualName = person.getName();
+        String actualMaritalStatus = person.getMaritalStatus();
+        String actualGender = person.getGender();
+        Date actualBirthDay = person.getBirthDay();
+
+        Assert.assertEquals(expectedAge, actualAge);
+        Assert.assertEquals(expectedName, actualName);
+        Assert.assertEquals(expectedAge, actualAge);
+        Assert.assertEquals(expectedGender, actualGender);
+        Assert.assertEquals(expectedMaritalStatus, actualMaritalStatus);
+    }
+
+    @Test
+    public void testSetMaritalStatus() {
+        // Given
+        Person person = new Person();
+        String expected = "Married";
+
+        // When
+        person.setMaritalStatus(expected);
+        String actual = person.getMaritalStatus();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetGender() {
+        // Given
+        Person person = new Person();
+        String expected = "Female";
+
+        // When
+        person.setGender(expected);
+        String actual = person.getGender();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetBirthDate() {
+        // Given
+        Person person = new Person();
+        Date expected = new Date(9);
+
+        // When
+        person.setBirthDay(expected);
+        Date actual = person.getBirthDay();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+
 }
